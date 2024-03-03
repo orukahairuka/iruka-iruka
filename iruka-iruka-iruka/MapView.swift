bb2cfe507282197b8d5f7ad64eb70e3b85eaed01
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-03 20:56:33 +0900

    first commit

---
+++ iruka-iruka-iruka/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  iruka-iruka-iruka
+//
+//  Created by 櫻井絵理香 on 2024/03/03.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
