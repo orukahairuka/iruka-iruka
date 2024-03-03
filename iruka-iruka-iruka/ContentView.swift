1276fb56d11bfbaeecb0de54475c0f231b78e802
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-03 20:53:57 +0900

    Initial Commit

---
+++ iruka-iruka-iruka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iruka-iruka-iruka
+//
+//  Created by 櫻井絵理香 on 2024/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iruka-iruka-iruka/iruka_iruka_irukaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iruka_iruka_irukaApp.swift
+//  iruka-iruka-iruka
+//
+//  Created by 櫻井絵理香 on 2024/03/03.
+//
+
+import SwiftUI
+
+@main
+struct iruka_iruka_irukaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
